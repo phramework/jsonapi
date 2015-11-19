59f612cc141ea6adcf413a59ad9e5e78306e0191
Xenofon Spafaridis <nohponex@gmail.com>
2015-11-19 13:42:33 +0200

    Initial commit

---
+++ tests/src/JSONAPI/RelationshipTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Phramework\JSONAPI;
+
+use \Phramework\Phramework;
+
+class Relationship extends \PHPUnit_Framework_TestCase
+{
+
+    private $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     * @todo update base
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+
+    }
+}
