b9a1cc7435d7b08e3568657844cdff43d63c9298
Xenofon Spafaridis <nohponex@gmail.com>
2016-01-02 13:38:00 +0200

    Update doc

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Phramework\\JSONAPI\\Controller"],["c","Phramework\\JSONAPI\\Controller\\Base"],["c","Phramework\\JSONAPI\\Controller\\DELETE"],["c","Phramework\\JSONAPI\\Controller\\GET"],["c","Phramework\\JSONAPI\\Controller\\GETById"],["c","Phramework\\JSONAPI\\Controller\\PATCH"],["c","Phramework\\JSONAPI\\Controller\\POST"],["c","Phramework\\JSONAPI\\Controller\\POST\\QueueItem"],["c","Phramework\\JSONAPI\\Controller\\Relationships"],["c","Phramework\\JSONAPI\\Model"],["c","Phramework\\JSONAPI\\Relationship"],["c","Phramework\\JSONAPI\\Viewers\\JSONAPI"]];
